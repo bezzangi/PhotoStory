5b71bcc391e052f387f7e1b970772b3e370bb838
Younghwa Park <bezzangi@gmail.com>
2017-08-24 02:12:32 +0900

    Camera source...

--- PhotoStory/CameraViewController.swift
+++ PhotoStory/CameraViewController.swift
@@ -6,14 +6,42 @@
 //  Copyright © 2017 Bezzangi. All rights reserved.
 //
 
+
+// http://jamesonquave.com/blog/taking-control-of-the-iphone-camera-in-ios-8-with-swift-part-1/
+// https://medium.com/@rizwanm/https-medium-com-rizwanm-swift-camera-part-1-c38b8b773b2
+
+
 import UIKit
+import AVFoundation
+
+
+
 
 class CameraViewController: UIViewController {
 
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var captureDevice : AVCaptureDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        for device in devices! {
+            // Make sure this particular device supports video
+            if ((device as AnyObject).hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if((device as AnyObject).position == AVCaptureDevicePosition.back) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        beginSession()
+                    }
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +49,63 @@ class CameraViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func beginSession() {
+        
+        var err : NSError? = nil
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        } catch {
+        }
+        
+        
+        if err != nil {
+        //    println("error: \(err?.localizedDescription)")
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer!)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
 
+    func configureDevice() {
+        if let device = captureDevice {
+            do {
+             try
+            device.lockForConfiguration()
+            device.focusMode = .locked
+            device.unlockForConfiguration()
+            } catch{
+                
+            }
+        }
+    }
+    
+    func focusTo(value : Float) {
+        if let device = captureDevice {
+            do { try    if(device.lockForConfiguration() != nil) {
+                device.setFocusModeLockedWithLensPosition(value, completionHandler: { (time) -> Void in
+                    //
+                })
+                device.unlockForConfiguration()
+            }
+        }
+    }
+    
+    let screenWidth = UIScreen.mainScreen().bounds.size.width
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        var anyTouch = touches.anyObject() as UITouch
+        var touchPercent = anyTouch.locationInView(self.view).x / screenWidth
+        focusTo(Float(touchPercent))
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        var anyTouch = touches.anyObject() as UITouch
+        var touchPercent = anyTouch.locationInView(self.view).x / screenWidth
+        focusTo(Float(touchPercent))
+    }
+    
+    
     /*
     // MARK: - Navigation
 
